e0b2a24a662367130921b2800367f4d509933c8e
Derek Stavis <dekestavis@gmail.com>
2015-12-06 12:14:00 -0200

    login: Add validation and an error dialog

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,13 +1,28 @@
-webgarden.controller('LoginCtrl', function (server, $scope, $state, $http, user) {
+webgarden.controller('LoginCtrl', function (server, $scope, $state, $http, user, $mdDialog) {
   $scope.user = {};
 
   function gotoPlants() {
     $state.go('user.plants');
   }
 
-  $scope.login = function () {
+  function cleanFields() {
+    $scope.user.user = '';
+    $scope.user.pass = '';
+  }
+
+  function loginError() {
+    console.log(event)
+    $mdDialog.show(
+      $mdDialog.confirm()
+        .title('Login Error')
+        .ok('Try Again'))
+      .then(cleanFields);
+  }
+
+  $scope.login = function (event) {
     $http.post(server.baseUrl + '/session', $scope.user)
       .then(R.prop('data'))
-      .then(gotoPlants);
+      .then(gotoPlants)
+      .catch(loginError);
   }
 });
