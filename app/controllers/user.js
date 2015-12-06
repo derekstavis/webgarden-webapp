8e788ce8d67cfddf60d3f309d89bb731e9275766
Derek Stavis <dekestavis@gmail.com>
2015-12-05 22:27:32 -0200

    Add a loader indicator

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,3 +1,13 @@
 webgarden.controller('UserCtrl', function ($state, $scope, user) {
   $scope.user = user;
+  $scope.$on('$stateChangeStart', function(event, toState) {
+    if (toState.resolve) {
+      $scope.loading = true;
+    }
+  });
+  $scope.$on('$stateChangeSuccess', function(event, toState) {
+    if (toState.resolve) {
+      $scope.loading = false;
+    }
+  });
 });
